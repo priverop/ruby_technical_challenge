1ba9a3451e9bdd5f0c83345daa557ade3390a8f2
priverop <priverop@users.noreply.github.com>
2026-03-19 19:58:11 +0100

    fix(main): fix number of arguments

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ require 'bundler/setup'
 require 'debug'
 require_relative 'lib/travel_manager'
 
-if ARGV.length < 1
+if ARGV.length != 1
   puts 'Wrong number of arguments. Usage: main.rb input.txt'
   exit 0
 end
@@ -17,4 +17,4 @@ input_reservations = ARGV[0]
 
 result = TravelManager.itinerary(input_reservations)
 
-p result
+pp result
