dc7688d50a741efc05515f847524e3c441efcca5
priverop <priverop@users.noreply.github.com>
2026-03-20 17:07:47 +0100

    test: add basic tests for travel_manager

---
+++ spec/travel_manager_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'travel_manager'
+
+RSpec.describe TravelManager do
+  let(:fixtures_path) { File.join(File.expand_path(__dir__), 'fixtures') }
+  let(:input_file) { File.join(fixtures_path, 'valid_input.txt') }
+  let(:output_file) { File.read(File.join(fixtures_path, 'valid_output.txt')) }
+
+  describe '.itinerary' do
+    context 'with valid input file and based' do
+      it 'returns the right itineraries' do
+        based = 'SVQ'
+
+        result = described_class.itinerary(input_file, based)
+
+        expect(result).to match(output_file)
+      end
+    end
+
+    context 'with valid input file, and wrong based' do
+      it 'returns ERROR message' do
+        based = 'SVG'
+
+        result = described_class.itinerary(input_file, based)
+
+        expect(result).to eq('ERROR!')
+      end
+    end
+  end
+end
