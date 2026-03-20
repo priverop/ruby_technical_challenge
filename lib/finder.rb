31b5b7cfb8a4a3d677c5c9314df303b5dbeb98fa
priverop <priverop@users.noreply.github.com>
2026-03-20 13:59:52 +0100

    feat(finder): finish linking segments

--- lib/finder.rb
+++ lib/finder.rb
@@ -1,23 +1,30 @@
 # frozen_string_literal: true
 
+# Links segments to each other to make itineraries
 class Finder
   def self.find(segments, based)
-    sorted = []
-    # TODO: en vez de first, se hace un bucle, que llama al LINKER
-    previous = segments.select{ |segment| segment.from == based }
-                      .sort_by { |segment| segment.date_from }
-                      .first # TODO: maybe instead o getting the first one, we should
+    # Gets all the segments that start in the based location
+    based_segments = segments.select { |segment| segment.from == based }
+                             .sort_by(&:date_from)
 
+    based_segments.map do |based_start|
+      linked_segments(based_start, segments)
+    end
+  end
+
+  # Gets all the linked segments starting from the "previous" segment (which is the based_segment)
+  def self.linked_segments(previous, segments)
+    sorted = []
     loop do
       sorted.push(previous)
-      previous = link(segments, previous)
+      previous = find_link(segments, previous)
       break if previous.nil?
     end
-
     sorted
   end
 
-  def self.link(segments, previous)
-    segments.select{ |segment| segment.from == previous.to && segment.date_from == previous.date_to }.first
+  # Gets the next linked segment of the "previous" segment
+  def self.find_link(segments, previous)
+    segments.select { |segment| segment.from == previous.to && segment.date_from == previous.date_to }.first
   end
 end
