94416162a41e66b58766a696be059c553508b92c
priverop <priverop@users.noreply.github.com>
2026-03-23 17:22:19 +0100

    chore(spec): activate simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,12 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+SimpleCov.profiles.define 'exclude_spec_files' do
+  add_filter 'spec' # Don't include spec files
+end
+
+SimpleCov.start 'exclude_spec_files'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
