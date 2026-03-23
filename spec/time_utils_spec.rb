9043535adc8d55882dcc8cbeec52f7560bed3b07
priverop <priverop@users.noreply.github.com>
2026-03-23 20:43:23 +0100

    test(time_utils): add unit tests

--- spec/time_utils_spec.rb
+++ spec/time_utils_spec.rb
@@ -27,6 +27,28 @@ RSpec.describe TimeUtils do
       end
     end
 
+    context 'when the first date is earlier than the second' do
+      it 'returns negative' do
+        a = Time.new(2023, 2, 15, 0, 0)
+        b = Time.new(2023, 2, 16, 12, 10)
+
+        result = described_class.hours_difference(a, b)
+
+        expect(result).to be < 0
+      end
+    end
+
+    context 'when same days but different month' do
+      it 'returns value bigger than 24' do
+        a = Time.new(2026, 3, 2, 9, 0)
+        b = Time.new(2026, 4, 2, 9, 0)
+
+        result = described_class.hours_difference(b, a)
+
+        expect(result).to be > 24
+      end
+    end
+
     context 'when the dates are the same' do
       it 'returns 0' do
         a = Time.new(2026, 3, 2, 9, 30)
