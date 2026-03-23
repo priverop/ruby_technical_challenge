d968c4c8ecbbb1870089c02b6efa0f7a1c9d3ad7
priverop <priverop@users.noreply.github.com>
2026-03-23 20:43:11 +0100

    test(finder): add finder unit tests

--- spec/finder_spec.rb
+++ spec/finder_spec.rb
@@ -4,52 +4,182 @@ require 'spec_helper'
 require 'finder'
 
 RSpec.describe Finder do
-  describe '.find_links' do
-    context 'when' do
-      skip 'TBI'
-    end
+  let(:unsorted_segments) do
+    [
+      Segment.new('Flight', 'SVQ', 'BCN', '2023-03-02', '2023-03-02', '06:40', '09:10'),
+      Segment.new('Hotel', 'BCN', 'BCN', '2023-01-05', '2023-01-10', nil, nil),
+      Segment.new('Flight', 'SVQ', 'BCN', '2023-01-05', '2023-01-05', '20:40', '22:10'),
+      Segment.new('Flight', 'BCN', 'SVQ', '2023-01-10', '2023-01-10', '10:30', '11:50'),
+      Segment.new('Train', 'SVQ', 'MAD', '2023-02-15', '2023-02-15', '9:30', '11:00'),
+      Segment.new('Train', 'MAD', 'SVQ', '2023-02-17', '2023-02-17', '17:00', '19:30'),
+      Segment.new('Hotel', 'MAD', 'MAD', '2023-02-15', '2023-02-17', nil, nil),
+      Segment.new('Train', 'BCN', 'NYC', '2023-03-02', '2023-03-02', '15:00', '22:45')
+    ]
+  end
+
+  # Trips have sorted segments
+  let(:trip_with_hotel) do
+    [
+      Segment.new('Flight', 'SVQ', 'BCN', '2023-01-05', '2023-01-05', '20:40', '22:10'),
+      Segment.new('Hotel', 'BCN', 'BCN', '2023-01-05', '2023-01-10', nil, nil),
+      Segment.new('Flight', 'BCN', 'SVQ', '2023-01-10', '2023-01-10', '10:30', '11:50')
+    ]
+  end
+
+  let(:trip_without_hotel_same_day) do
+    [
+      Segment.new('Flight', 'SVQ', 'MAD', '2023-03-02', '2023-03-02', '06:40', '09:10'),
+      Segment.new('Flight', 'MAD', 'NYC', '2023-03-02', '2023-03-02', '15:00', '22:45')
+    ]
+  end
+
+  let(:trip_without_hotel_different_day) do
+    [
+      Segment.new('Flight', 'SVQ', 'MAD', '2023-03-02', '2023-03-02', '06:40', '09:10'),
+      Segment.new('Flight', 'MAD', 'NYC', '2023-03-03', '2023-03-03', '7:00', '19:45')
+    ]
   end
 
   describe '.sorted_segments' do
     skip 'TBI'
   end
 
+  ###############
+  # UNIT TESTS: #
+  ###############
+
+  describe '.find_links' do
+    context 'when the dates are the same' do
+      it 'returns the correct next segment' do
+        previous = Segment.new('Flight', 'SVQ', 'BCN', '2023-01-05', '2023-01-05', '20:40', '22:10')
+        segments = [Segment.new('Hotel', 'BCN', 'BCN', '2023-01-05', '2023-01-10', nil, nil)]
+
+        result = described_class.find_link(segments, previous)
+        expect(result).to have_attributes(
+          type: 'Hotel',
+          from: 'BCN',
+          to: 'BCN',
+          datetime_from: TimeUtils.date_to_time('2023-01-05'),
+          datetime_to: TimeUtils.date_to_time('2023-01-10')
+        )
+      end
+    end
+
+    context 'when the dates are different, but < 24h' do
+      it 'returns the correct next segment' do
+        previous = Segment.new('Train', 'SVQ', 'MAD', '2023-02-15', '2023-02-16', '15:00', '18:10')
+        segments = unsorted_segments
+
+        result = described_class.find_link(segments, previous)
+        expect(result).to have_attributes(
+          type: 'Train',
+          from: 'MAD',
+          to: 'SVQ',
+          datetime_from: TimeUtils.datetime_to_time('2023-02-17', '17:00'),
+          datetime_to: TimeUtils.datetime_to_time('2023-02-17', '19:30')
+        )
+      end
+    end
+
+    context 'when the dates are different, and > 24h' do
+      it 'returns nil' do
+        previous = Segment.new('Train', 'SVQ', 'MAD', '2023-02-15', '2023-02-16', '9:00', '12:10')
+        segments = unsorted_segments
+
+        result = described_class.find_link(segments, previous)
+        expect(result).to be_nil
+      end
+    end
+  end
+
+  describe '.check_connection' do
+    context 'when the flights time are < 24' do
+      it 'returns true and sets the first segment as is_connection' do
+        previous = trip_without_hotel_different_day.first
+        next_segment = trip_without_hotel_different_day.last
+
+        expect(previous.connection?).to be(false)
+        result = described_class.check_connection(previous, next_segment)
+        expect(result).to be(true)
+        expect(previous.connection?).to be(true)
+      end
+    end
+
+    context 'when the flights time are > 24' do
+      it 'returns false and does nothing' do
+        previous = trip_with_hotel.first
+        next_segment = trip_with_hotel.last
+
+        expect(previous.connection?).to be(false)
+        result = described_class.check_connection(previous, next_segment)
+        expect(result).to be(false)
+        expect(previous.connection?).to be(false)
+      end
+    end
+
+    context 'when only one is a flight' do
+      it 'returns false and does_nothing' do
+        previous = trip_with_hotel.first
+        next_segment = trip_with_hotel.at(1)
+
+        expect(previous.connection?).to be(false)
+        result = described_class.check_connection(previous, next_segment)
+        expect(result).to be(false)
+        expect(previous.connection?).to be(false)
+      end
+    end
+
+    context 'when they are the same flight' do
+      it 'returns false and does_nothing' do
+        previous = trip_with_hotel.first
+        next_segment = trip_with_hotel.first
+
+        expect(previous.connection?).to be(false)
+        result = described_class.check_connection(previous, next_segment)
+        expect(result).to be(false)
+        expect(previous.connection?).to be(false)
+      end
+    end
+  end
+
   describe '.find_trip_destiny' do
-    let(:with_hotel) do
-      [
-        Segment.new('Flight', 'SVQ', 'BCN', '2023-01-05', '2023-01-05', '20:40', '22:10'),
-        Segment.new('Hotel', 'BCN', 'BCN', '2023-01-05', '2023-01-10', nil, nil),
-        Segment.new('Flight', 'BCN', 'SVQ', '2023-01-10', '2023-01-10', '10:30', '11:50')
-      ]
-    end
-
-    let(:no_hotel) do
-      [
-        Segment.new('Flight', 'SVQ', 'MAD', '2023-03-02', '2023-03-02', '06:40', '09:10'),
-        Segment.new('Flight', 'MAD', 'NYC', '2023-03-02', '2023-03-02', '15:00', '22:45')
-      ]
-    end
-
     context 'when no connection flights, and hotel' do
       it 'returns the first destiny' do
-        result = described_class.find_trip_destiny(with_hotel)
+        result = described_class.find_trip_destiny(trip_with_hotel)
 
         expect(result).to be('BCN')
       end
     end
 
-    context 'when no connection flights, and no hotel' do
+    context 'when no connection flights, flights same day' do
       it 'returns the first destiny' do
-        result = described_class.find_trip_destiny(no_hotel)
+        result = described_class.find_trip_destiny(trip_without_hotel_same_day)
 
         expect(result).to be('MAD')
       end
     end
 
-    context 'when connection flights, and no hotel' do
+    context 'when no connection flights, no hotel, different days (layover)' do
       it 'returns the first destiny' do
-        no_hotel.first.is_connection = true
-        result = described_class.find_trip_destiny(no_hotel)
+        result = described_class.find_trip_destiny(trip_without_hotel_different_day)
+
+        expect(result).to be('MAD')
+      end
+    end
+
+    context 'when connection flights, flights same day, <24h' do
+      it 'returns the first destiny' do
+        trip_without_hotel_same_day.first.is_connection = true
+        result = described_class.find_trip_destiny(trip_without_hotel_same_day)
+
+        expect(result).to be('NYC')
+      end
+    end
+
+    context 'when connection flights, no hotel, different days (layover)' do
+      it 'returns the first destiny' do
+        trip_without_hotel_different_day.first.is_connection = true
+        result = described_class.find_trip_destiny(trip_without_hotel_different_day)
 
         expect(result).to be('NYC')
       end
