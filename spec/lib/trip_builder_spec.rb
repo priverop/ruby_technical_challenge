b925a5e7a6a0fb46df16882ece83bd78effc6d85
priverop <priverop@users.noreply.github.com>
2026-03-31 15:42:19 +0200

    test(trip_builder): check attributes instead of object similatiry

--- spec/lib/trip_builder_spec.rb
+++ spec/lib/trip_builder_spec.rb
@@ -33,6 +33,12 @@ RSpec.describe TripBuilder do
     ]
   end
 
+  let(:overnight_flight) do
+    Segment.new(type: 'Flight', from: 'SVQ', to: 'MAD',
+                datetime_from: TimeUtils.to_time('2023-02-15', '15:00'),
+                datetime_to: TimeUtils.to_time('2023-02-16', '18:10'))
+  end
+
   describe '.build' do
     context 'when passing valid segments' do
       it 'returns a valid array of Trips' do
@@ -102,9 +108,17 @@ RSpec.describe TripBuilder do
 
         expect(result.count).to eq(2)
         expect(result.first.destination).to eq('LAX')
-        expect(result.first.sorted_segments).to eq([unsorted_segments.first])
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'NYC', to: 'LAX',
+          datetime_from: TimeUtils.to_time('2023-03-02', '06:40'),
+          datetime_to: TimeUtils.to_time('2023-03-02', '09:10'),
+        )
         expect(result.last.destination).to eq('JFK')
-        expect(result.last.sorted_segments).to eq([unsorted_segments[2]])
+        expect(result.last.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'NYC', to: 'JFK',
+          datetime_from: TimeUtils.to_time('2023-06-02', '15:00'),
+          datetime_to: TimeUtils.to_time('2023-06-02', '22:45')
+        )
       end
     end
 
@@ -115,35 +129,56 @@ RSpec.describe TripBuilder do
                                 datetime_to: TimeUtils.to_time('2023-01-05', '22:10'))]
 
         result = described_class.build(segments, 'SVQ')
-        expect(result).to eq([Trip.new('BCN', segments)])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('BCN')
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'SVQ', to: 'BCN',
+          datetime_from: TimeUtils.to_time('2023-01-05', '20:40'),
+          datetime_to: TimeUtils.to_time('2023-01-05', '22:10')
+        )
       end
     end
 
     context 'when the dates are different, but < 24h (connection flights)' do
       it 'returns the trip destination to SVQ' do
-        segments = [Segment.new(type: 'Flight', from: 'SVQ', to: 'MAD',
-                                datetime_from: TimeUtils.to_time('2023-02-15', '15:00'),
-                                datetime_to: TimeUtils.to_time('2023-02-16', '18:10')),
+        segments = [overnight_flight,
                     Segment.new(type: 'Flight', from: 'MAD', to: 'SVQ',
                                 datetime_from: TimeUtils.to_time('2023-02-17', '17:00'),
                                 datetime_to: TimeUtils.to_time('2023-02-17', '19:30'))]
 
         result = described_class.build(segments, 'SVQ')
-        expect(result).to eq([Trip.new('SVQ', segments)])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('SVQ')
+        expect(result.first.sorted_segments.count).to eq(2)
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'SVQ', to: 'MAD',
+          is_connection: true
+        )
+        expect(result.first.sorted_segments.last).to have_attributes(
+          type: 'Flight', from: 'MAD', to: 'SVQ',
+          is_connection: nil
+        )
       end
     end
 
     context 'when the dates are different, and > 24h (no connection flights)' do
       it 'returns the trip destination to MAD' do
-        segments = [Segment.new(type: 'Flight', from: 'SVQ', to: 'MAD',
-                                datetime_from: TimeUtils.to_time('2023-02-15', '15:00'),
-                                datetime_to: TimeUtils.to_time('2023-02-16', '18:10')),
+        segments = [overnight_flight,
                     Segment.new(type: 'Flight', from: 'MAD', to: 'SVQ',
                                 datetime_from: TimeUtils.to_time('2023-02-17', '18:20'),
                                 datetime_to: TimeUtils.to_time('2023-02-17', '19:30'))]
 
         result = described_class.build(segments, 'SVQ')
-        expect(result).to eq([Trip.new('MAD', [segments.first])])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('MAD')
+        expect(result.first.sorted_segments.count).to eq(1)
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'SVQ', to: 'MAD',
+          is_connection: nil
+        )
       end
     end
 
@@ -157,22 +192,42 @@ RSpec.describe TripBuilder do
                                 datetime_to: TimeUtils.to_time('2023-02-17', '19:30'))]
 
         result = described_class.build(segments, 'SVQ')
-        expect(result).to eq([Trip.new('SVQ', segments)])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('SVQ')
+        expect(result.first.sorted_segments.count).to eq(2)
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'SVQ', to: 'MAD',
+          is_connection: true
+        )
+        expect(result.first.sorted_segments.last).to have_attributes(
+          type: 'Flight', from: 'MAD', to: 'SVQ',
+          is_connection: nil
+        )
       end
     end
 
     # requirements say only two flights can be considered connections
     context 'when there is a connection travel, but its not a flight' do
       it 'returns the correct next segment' do
-        segments = [Segment.new(type: 'Flight', from: 'SVQ', to: 'MAD',
-                                datetime_from: TimeUtils.to_time('2023-02-15', '15:00'),
-                                datetime_to: TimeUtils.to_time('2023-02-16', '18:10')),
+        segments = [overnight_flight,
                     Segment.new(type: 'Train', from: 'MAD', to: 'SVQ',
                                 datetime_from: TimeUtils.to_time('2023-02-17', '17:00'),
                                 datetime_to: TimeUtils.to_time('2023-02-17', '19:30'))]
 
         result = described_class.build(segments, 'SVQ')
-        expect(result).to eq([Trip.new('MAD', segments)])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('MAD')
+        expect(result.first.sorted_segments.count).to eq(2)
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'SVQ', to: 'MAD',
+          is_connection: false
+        )
+        expect(result.first.sorted_segments.last).to have_attributes(
+          type: 'Train', from: 'MAD', to: 'SVQ',
+          is_connection: nil
+        )
       end
     end
 
@@ -187,7 +242,18 @@ RSpec.describe TripBuilder do
                                 datetime_to: TimeUtils.to_time('2023-02-15', '20:00'))]
 
         result = described_class.build(segments, 'SVQ')
-        expect(result).to eq([Trip.new('SVQ', segments)])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('SVQ')
+        expect(result.first.sorted_segments.count).to eq(2)
+        expect(result.first.sorted_segments.first).to have_attributes(
+          type: 'Flight', from: 'SVQ', to: 'MAD',
+          is_connection: true
+        )
+        expect(result.first.sorted_segments.last).to have_attributes(
+          type: 'Flight', from: 'MAD', to: 'SVQ',
+          is_connection: nil
+        )
       end
     end
 
@@ -205,10 +271,23 @@ RSpec.describe TripBuilder do
                       datetime_to: TimeUtils.to_time('2023-03-02', '17:45'))
         ]
 
-        expected_destination = 'LAX'
-
         result = described_class.build(segments, 'NYC')
-        expect(result).to eq([Trip.new(expected_destination, segments)])
+
+        expect(result.count).to eq(1)
+        expect(result.first.destination).to eq('LAX')
+        expect(result.first.sorted_segments.count).to eq(3)
+        expect(result.first.sorted_segments[0]).to have_attributes(
+          type: 'Flight', from: 'NYC', to: 'JFK',
+          is_connection: true
+        )
+        expect(result.first.sorted_segments[1]).to have_attributes(
+          type: 'Flight', from: 'JFK', to: 'ORD',
+          is_connection: true
+        )
+        expect(result.first.sorted_segments[2]).to have_attributes(
+          type: 'Flight', from: 'ORD', to: 'LAX',
+          is_connection: nil
+        )
       end
     end
   end
